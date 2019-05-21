d9d9781523aa021f16331ac3b6d09dcfeff61fe5
Jonathan Constant <johnny45gotu@outlook.com>
2019-05-21 18:28:20 -0400

    Pillow added upload image test added

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 from django.conf import settings
+import uuid
+import os
 
 
 class UserManager(BaseUserManager):
@@ -72,8 +74,16 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
+
+    def recipe_image_file_path(instance, filename):
+            """Generate file path for new recipe image"""
+        ext = filename.split('.')[-1]
+        filename = f'{uuid.uuid4()}.{ext}'
+
+        return os.path.join('uploads/recipe/', filename)
